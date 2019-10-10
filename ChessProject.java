a9ed8ca726727dac37c93eeb54f7917628aeb007
Ashish <blr.ashu@gmail.com>
2019-10-10 12:08:44 +0100

    added rook

--- ChessProject.java
+++ ChessProject.java
@@ -224,6 +224,95 @@ public class ChessProject extends JFrame implements MouseListener, MouseMotionLi
     if(pieceName.equals("BlackQueen")){
       validMove = true;
     }
+    else if(pieceName.contains("Rook")){
+      Boolean intheway = false;
+      if(((landingX <0) || (landingX > 7)) || ((landingY < 0) || (landingY > 7))){
+        validMove = false;
+      }
+      else{
+        if(((Math.abs(startX-landingX)!=0)&&(Math.abs(startY-landingY)==0))|| ((Math.abs(startX-landingX)==0)&&(Math.abs(landingY-startY)!=0))){
+          if(Math.abs(startX-landingX)!=0){
+            xMovement = Math.abs(startX-landingX);
+            if(startX-landingX >0){
+              for(int i=0;i< xMovement;i++){
+                if(piecePresent(initialX-(i*75),e.getY())){
+                  intheway = true;
+                  break;
+                }
+                else{
+                  intheway = false;
+                }
+              }
+            }
+            else{
+              for(int i=0;i<xMovement;i++){
+                if(piecePresent(initialX+(i*75),e.getY())){
+                  intheway = true;
+                  break;
+                }
+                else{
+                  intheway = false;
+                }
+              }
+            }
+          }
+          else{
+            yMovement = Math.abs(startY-landingY);
+            if(startY-landingY > 0){
+              for(int i=0;i<yMovement;i++){
+                if(piecePresent(e.getX(),initialY-(i*75))){
+                  intheway = true;
+                  break;
+                }
+                else{
+                  intheway = false;
+                }
+              }
+            }
+            else{
+              for(int i=0;i<yMovement;i++){
+                if(piecePresent(e.getX(),initialY+(i*75))){
+                  intheway = true;
+                  break;
+                }
+                else{
+                  intheway = false;
+                }
+              }
+            }
+          }
+          if(intheway){
+            validMove= false;
+          }
+          else{
+            if(piecePresent(e.getX(),e.getY())){
+              if(pieceName.contains("White")){
+                if(checkWhiteOponent(e.getX(),e.getY())){
+                  validMove = true;
+                }
+                else{
+                  validMove=false;
+                }
+              }
+              else{
+                if(checkBlackOponent(e.getX(),e.getY())){
+                  validMove = true;
+                }
+                else{
+                  validMove = false;
+                }
+              }
+            }
+            else{
+              validMove = true;
+            }
+          }
+        }
+        else{
+          validMove = false;
+        }
+      }
+    }
     else if(pieceName.contains("Knight")){//Sample Workbook tutorial
       if(((landingX < 0) || (landingX > 7)) ||((landingY < 0 ) || (landingY > 7))){
         validMove = false;
@@ -329,15 +418,7 @@ public class ChessProject extends JFrame implements MouseListener, MouseMotionLi
          }
       }
     }
-    else if(pieceName.contains("Rook")){
-      Boolean intheway = false;
-      if(((landingX <0) || (landingX > 7)) || ((landingY < 0) || (landingY > 7))){
-        validMove = false;
-      }
-      else{
-        if(((Math.abs(startX-landingX)!=0)&&(Math.abs(startY-landingY)==0))|| (()))
-      }
-    }
+
     else if(pieceName.equals("BlackPawn") ){
       if(startY == 6){//This is where the pawn is making its first move --- Video Tutorial
         if(((yMovement ==1 ) || (yMovement ==2 ))&& (startY > landingY) && (xMovement ==0)){
